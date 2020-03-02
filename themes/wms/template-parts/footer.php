0ea26532b69588d76925e9adf8a90aecb3b03ebb
PXJesse <jessetraas@gmail.com>
2020-03-02 03:53:59 +0100

    Updated filler content

--- themes/wms/template-parts/footer.php
+++ themes/wms/template-parts/footer.php
@@ -3,6 +3,6 @@
         <p>© Wim Steenbakker, <?= date("Y") ?>. All rights reserved.</p>
     </div>
     <div class="right">
-        <p><i class="far fa-envelope fa-lg"></i>contact@wimsteenbakker.nl</p><p><i class="fas fa-phone fa-lg"></i>+31 6 6969 4200</p>
+        <p><i class="far fa-envelope fa-lg"></i>contact@wimsteenbakker.nl</p><p><i class="fas fa-phone fa-lg"></i>+31 6 1234 5678</p>
     </div>
 </footer>
